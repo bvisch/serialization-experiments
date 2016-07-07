e24d9878b700d1f6944116779a1ae254f692a2b0
bvisch <bvisch@gmail.com>
2016-07-06 20:20:38 -0400

    Serializing to binary and back

--- Serializer/Serializer.cpp
+++ Serializer/Serializer.cpp
@@ -1,5 +1,30 @@
 #include <vector>
 #include <iostream>
+#include <string>
+
+//class Packet {
+//	unsigned char* 
+//};
+
+class Serializable {
+
+public:
+	int i;
+	std::string s;
+
+	unsigned char* to_bytes() {
+		unsigned char* packet = new unsigned char[sizeof(size_t) + 50];
+		unsigned char* placeholder = packet;
+		size_t len = s.length();
+		*reinterpret_cast<size_t*>(packet) = s.length(); 
+		len = *reinterpret_cast<size_t*>(packet); packet += sizeof(size_t);
+		*reinterpret_cast<int*>(packet) = i; packet += sizeof(int);
+
+		for (auto i = 0; i < s.length(); ++i) *reinterpret_cast<char*>(packet++) = s[i];
+
+		return placeholder;
+	}
+};
 
 template <typename T, typename ...Types>
 std::vector<unsigned char> serialize(T first, Types ... args) {
@@ -29,6 +54,20 @@ int main()
 
 	std::cin >> i >> c;
 	std::vector<unsigned char> buffer = serialize(i, c);
-	std::cout << (int)buffer[0] << (char)buffer[1];
+	std::cout << (int)buffer[0] << (char)buffer[1] << std::endl << std::endl;
+
+
+	Serializable s{ 1, "oh shit waddup" };
+
+	unsigned char* serialized = s.to_bytes();
+	size_t length = *reinterpret_cast<size_t*>(serialized); serialized += sizeof(size_t);
+	int j = *reinterpret_cast<int*>(serialized); serialized += sizeof(int);
+	char* c_string = reinterpret_cast<char*>(serialized);
+	std::string str;
+	str.append(c_string, c_string + length);
+
+	std::cout << j << str;
+
+	delete[] serialized;
 }
 
